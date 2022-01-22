d414f39eebe2c242618fefb59f52aa27ed0b625b
rohit-manethiya <rohitand07@gmail.com>
2022-01-22 23:00:43 +0530

    payment method api

--- index.js
+++ index.js
@@ -1,46 +1,106 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require("express");
+const app = express();
+const port = 3001;
 
-const bids_model = require('./models/bids_model')
-const payment_methods_model = require('./models/payment_methods_model')
+const bids_model = require("./models/bids_model");
+const payment_methods_model = require("./models/payment_methods_model");
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger_output.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger_output.json");
 
-app.use(express.json())
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use(express.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers"
+  );
   next();
 });
 
-app.post('/bids', (req, res) => {
-  bids_model.create_bid(req.body)
-  .then(response => {
-    res.status(200).send(response);
-  })
-  .catch(error => {
-    console.log(error);
-    res.status(500).send(error);
-  })
-})
+app.post("/bids", (req, res) => {
+  bids_model
+    .create_bid(req.body)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
 
-app.post('/payment_methods', (req, res) => {
-  const body = req.body
-  payment_methods_model.create_payment_method(body)
-  .then(response => {
-    res.status(200).send(response);
-  })
-  .catch(error => {
-    console.log(error);
-    res.status(500).send(error);
-  })
-})
+app.post("/payment_methods", (req, res) => {
+  const body = req.body;
+  payment_methods_model
+    .create_payment_method(body)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
+
+app.get("/payment_methods", (req, res) => {
+  const body = req.body;
+  console.log("in get method");
+  payment_methods_model
+    .get_payment_method(body)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
+
+app.get("/payment_methods/:id", (req, res) => {
+  const id = req.params.id;
+  console.log("in get by id method");
+  payment_methods_model
+    .get_byId_payment_method(id)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
+
+app.patch("/payment_methods/:id", (req, res) => {
+  const resourceId = req.params.id;
+  const body = req.body;
+  payment_methods_model
+    .update_payment_method(body, resourceId)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
+
+app.delete("/payment_methods/:id", (req, res) => {
+  const id = req.params.id;
+  payment_methods_model
+    .delete_payment_method(id)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
 
 app.listen(port, () => {
-  console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
--- models/payment_methods_model.js
+++ models/payment_methods_model.js
@@ -1,19 +1,106 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 const dbprop = require("../dbprop.json");
 const pool = new Pool(dbprop);
 
 const create_payment_method = (body) => {
-  return new Promise(function(resolve, reject) {
-    const { qr_code, upi_id, mobile } = body
-    pool.query('INSERT INTO payment_methods (qr_code, upi_id, mobile) VALUES ($1, $2, $3) RETURNING *', [qr_code, upi_id, mobile], (error, results) => {
-      if (error) {
-        reject(error)
+  return new Promise(function (resolve, reject) {
+    const { qr_code, upi_id, mobile } = body;
+    pool.query(
+      "INSERT INTO payment_methods (qr_code, upi_id, mobile) VALUES ($1, $2, $3) RETURNING *",
+      [qr_code, upi_id, mobile],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(`A new payment method has been added placed: `);
       }
-      resolve(`A new payment method has been added placed: ${results.rows[0]}`)
-    })
-  })
-}
+    );
+  });
+};
+
+const get_payment_method = () => {
+  return new Promise(function (resolve, reject) {
+    pool.query("Select * from payment_methods", (error, results) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(results.rows);
+    });
+  });
+};
+
+const get_byId_payment_method = (id) => {
+  return new Promise(function (resolve, reject) {
+    console.log("getting by id", id);
+    pool.query(
+      "Select * from payment_methods where Id = $1",
+      [id],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(results);
+      }
+    );
+  });
+};
+
+const update_payment_method = (body, resourceId) => {
+  return new Promise(function (resolve, reject) {
+    console.log("body", body);
+    const { qr_code, upi_id, mobile } = body;
+    var updateQuery = "Update payment_methods set ";
+    var counter = 1;
+    var queryVarArry = [];
+    if (qr_code) {
+      updateQuery += "qr_code=$" + counter + ",";
+      queryVarArry.push(qr_code);
+      counter++;
+    }
+    if (upi_id) {
+      updateQuery += "upi_id=$" + counter + ",";
+      queryVarArry.push(upi_id);
+      counter++;
+    }
+    if (mobile) {
+      updateQuery += "mobile=$" + counter + "";
+      queryVarArry.push(mobile);
+      counter++;
+    }
+    updateQuery = updateQuery.substring(0, updateQuery.length - 1);
+    queryVarArry.push(resourceId);
+    pool.query(
+      updateQuery + " where id=$" + counter,
+      queryVarArry,
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(results);
+      }
+    );
+  });
+};
+
+const delete_payment_method = (id) => {
+  return new Promise(function (resolve, reject) {
+    pool.query(
+      "Delete from payment_methods where id=$1",
+      [id],
+      (error, results) => {
+        if (error) {
+          reject(error);
+        }
+        resolve(`payment method has been deleted with id`, id);
+      }
+    );
+  });
+};
 
 module.exports = {
   create_payment_method,
-}
+  get_payment_method,
+  update_payment_method,
+  delete_payment_method,
+  get_byId_payment_method,
+};
