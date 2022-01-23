291d862d2f9da3b3e62e63b1372062aea3c7e923
rohit-manethiya <rohitand07@gmail.com>
2022-01-23 22:10:50 +0530

    added venues model

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 3001;
 const bids_model = require("./models/bids_model");
 const payment_methods_model = require("./models/payment_methods_model");
 const users_model = require("./models/users_model");
+const venues_model = require("./models/venues_model");
 
 const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger_output.json");
@@ -162,6 +163,75 @@ app.delete("/users/:id", (req, res) => {
   const id = req.params.id;
   users_model
     .delete_user_method(id)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
+
+// VENUES
+app.post("/venues", (req, res) => {
+  const body = req.body;
+  venues_model
+    .create_venue_method(body)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
+
+app.get("/venues", (req, res) => {
+  const body = req.body;
+  console.log("in get method");
+  venues_model
+    .get_venue_method(body)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
+
+app.get("/venues/:id", (req, res) => {
+  const id = req.params.id;
+  console.log("in get by id method");
+  venues_model
+    .get_venueById_method(id)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
+
+app.patch("/venues/:id", (req, res) => {
+  const resourceId = req.params.id;
+  const body = req.body;
+  venues_model
+    .update_venue_method(body, resourceId)
+    .then((response) => {
+      res.status(200).send(response);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(error);
+    });
+});
+
+app.delete("/venues/:id", (req, res) => {
+  const id = req.params.id;
+  venues_model
+    .delete_venue_method(id)
     .then((response) => {
       res.status(200).send(response);
     })
